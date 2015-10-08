cab9b98232216e4595c0239c68d33dc3420ee784
Ismael Angelo A. Casimpan Jr <ismael.angelo@casimpan.com>
2015-10-08 09:00:43 +0000

    removed some other package that are not yet sure needed

--- puppet/modules/other_deps/manifests/init.pp
+++ puppet/modules/other_deps/manifests/init.pp
@@ -7,27 +7,6 @@ class other_deps {
 			'libpq-dev',
 			'g++',
 			'make',
-                        'git-core',
-                        'curl',
-                        'zlib1g-dev',
-                        'build-essential',
-                        'libssl-dev',
-                        'libreadline-dev',
-                        'libyaml-dev',
-                        'libsqlite3-dev', 
-                        'sqlite3',
-                        'libxml2-dev', 
-                        'libxslt1-dev',
-                        'libcurl4-openssl-dev',
-                        'python-software-properties',
-                        'libffi-dev',
-                        'vim',
-                        'expect',
-                        'debconf-utils',
-                        'build-essential',
-                        'zlib1g-dev',
-                        'libreadline6-dev',
-                        'libpcre3',
-                      ]
+            ]
   package { $other_deps_list: }
 }
