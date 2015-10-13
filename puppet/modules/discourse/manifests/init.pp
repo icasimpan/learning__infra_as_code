f59ef05dedf775a82316fc960753dd0797ae4ade
Ismael Casimpan <ismael.angelo@casimpan.com>
2015-10-13 17:47:20 +0800

    made sure discourse clone will not timeout

--- puppet/modules/discourse/manifests/init.pp
+++ puppet/modules/discourse/manifests/init.pp
@@ -1,14 +1,21 @@
 class discourse {
+  
+  ## git is only needed in getting the discourse source code from github
   package { 'git':
     ensure => 'present',
     require => Exec['apt-get update'],
   }
 
-
   ## Get the discourse source code...
   exec { 'gitclone_discourse':
     command => '/usr/bin/sudo /usr/bin/puppet apply /vagrant/puppet/modules/discourse/_workaround/get_discourse_code.pp  --modulepath=/vagrant/puppet/modules',
     require => Package['git'],
+    creates  => '/opt/discourse/COPYRIGHT.txt',
+    timeout  => '0',
+  }->
+  exec { 'ensure_discourse_source_ownerOK':
+    command => '/bin/chown -R vagrant:rvm /opt/discourse',
+    tries   => '5',
   }
   
   ##
@@ -16,11 +23,11 @@ class discourse {
   ##   1. Make sure the discourse app runs and is accessible from 
   ##      eth1 ip @port 80
   ##
-  ##    * cd /opt/discourse/ap
+  ##    * cd /opt/discourse/app
   ##    * bundle install
   ##    * bundle exec rake db:create db:migrate db:test:prepare
   ##
-  ##   2. Only above is running, deamonize
+  ##   2. Once above is running, deamonize
   ##
 
 }
