54f2b01971489778a6ddc7a834a8581a8dcceb72
Ismael Casimpan <ismael.angelo@casimpan.com>
2015-10-14 17:52:09 +0800

    commented-out for now the rvm installation + added notes that would be needed to automate via puppet

--- puppet/modules/ruby/manifests/init.pp
+++ puppet/modules/ruby/manifests/init.pp
@@ -25,6 +25,20 @@ class ruby {
                         'pkg-config',
                         'curl',
                         'build-essential',
+                        'libcurl4-openssl-dev',
+                        'apache2-prefork-dev',
+                        'libapr1-dev',
+                        'libaprutil1-dev',
+                        'libx11-dev',
+                        'libffi-dev',
+                        'tcl-dev',
+                        'tk-dev',
+                        'openssl',
+                        'libreadline6',
+                        'libreadline6-dev',
+                        'zlib1g',
+                        'zlib1g-dev',
+                        'libssl-dev',
             ]
   package { $ruby_deps_list: }
 
@@ -34,65 +48,65 @@ class ruby {
   }
 
   ## run rvm-installer
-  exec { 'rvm_installer_run':
-    command => '/usr/bin/sudo /usr/bin/puppet apply /vagrant/puppet/modules/ruby/_workaround/rvm_install.pp',
-  }
+  #exec { 'rvm_installer_run':
+  #  command => '/usr/bin/sudo /usr/bin/puppet apply /vagrant/puppet/modules/ruby/_workaround/rvm_install.pp',
+  #}
 
   ## create 'rvm' group ...
   ## and make 'vagrant' part of 'rvm' group
-  group { 'rvm':
-    ensure => 'present',
-  }->
-  user { 'vagrant':
-    ensure => 'present',
-    groups => 'rvm',
-  }
+  #group { 'rvm':
+  #  ensure => 'present',
+  #}->
+  #user { 'vagrant':
+  #  ensure => 'present',
+  #  groups => 'rvm',
+  #}
 
-  exec { 'rvm_install__libyaml':
-    environment => ['HOME=/home/vagrant'],
-    command => '/home/vagrant/.rvm/bin/rvm pkg install libyaml',
-    require => Exec['rvm_installer_run'],
-  }->
-  exec { 'ensure_rvmdir_correct_ownership00':
-    command => '/bin/chown -R vagrant:rvm /home/vagrant/.rvm',
-  }
+  #exec { 'rvm_install__libyaml':
+  #  environment => ['HOME=/home/vagrant'],
+  #  command => '/home/vagrant/.rvm/bin/rvm pkg install libyaml',
+  #  require => Exec['rvm_installer_run'],
+  #}->
+  #exec { 'ensure_rvmdir_correct_ownership00':
+  #  command => '/bin/chown -R vagrant:rvm /home/vagrant/.rvm',
+  #}
 
-  exec { 'rvm_reinstall_all':
-    environment => ['HOME=/home/vagrant'],
-    command => '/home/vagrant/.rvm/bin/rvm reinstall all --force',
-    require => Exec['rvm_installer_run'],
-  }->
-  exec { 'ensure_rvmdir_correct_ownership01':
-    command => '/bin/chown -R vagrant:rvm /home/vagrant/.rvm',
-  }
+  #exec { 'rvm_reinstall_all':
+  #  environment => ['HOME=/home/vagrant'],
+  #  command => '/home/vagrant/.rvm/bin/rvm reinstall all --force',
+  #  require => Exec['rvm_installer_run'],
+  #}->
+  #exec { 'ensure_rvmdir_correct_ownership01':
+  #  command => '/bin/chown -R vagrant:rvm /home/vagrant/.rvm',
+  #}
 
-  exec { 'rvm_install_223':
-    environment => ['HOME=/home/vagrant'],
-    command => '/home/vagrant/.rvm/bin/rvm install 2.2.3',
-    require => Exec['rvm_installer_run'],
-    timeout => '0',
-  }->
-  exec { 'ensure_rvmdir_correct_ownership02':
-    command => '/bin/chown -R vagrant:rvm /home/vagrant/.rvm',
-  }
+  #exec { 'rvm_install_223':
+  #  environment => ['HOME=/home/vagrant'],
+  #  command => '/home/vagrant/.rvm/bin/rvm install 2.2.3',
+  #  require => Exec['rvm_installer_run'],
+  #  timeout => '0',
+  #}->
+  #exec { 'ensure_rvmdir_correct_ownership02':
+  #  command => '/bin/chown -R vagrant:rvm /home/vagrant/.rvm',
+  #}
 
-  exec { 'rvm_use_223':
-    environment => ['HOME=/home/vagrant'],
-    command => 'rvm use 2.2.3 --default',
-    path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin', '/home/vagrant/.rvm/bin', '/home/vagrant/.rvm/bin'],
-    require => Exec['rvm_installer_run'],
-  }->
-  exec { 'ensure_rvmdir_correct_ownership03':
-    command => '/bin/chown -R vagrant:rvm /home/vagrant/.rvm',
-  }-> 
-  file { '/home/vagrant/.bash_profile':
-    ensure => 'present',
-  }->
-  file_line { 'write_rvm_default_homebashrc':
-    path  => '/home/vagrant/.bash_profile',
-    line  => 'rvm use 2.2.3 --default',
-    match => '^rvm\ use\ 2\.2\.3*',
-  }
+  #exec { 'rvm_use_223':
+  #  environment => ['HOME=/home/vagrant'],
+  #  command => 'rvm use 2.2.3 --default',
+  #  path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin', '/home/vagrant/.rvm/bin', '/home/vagrant/.rvm/bin'],
+  #  require => Exec['rvm_installer_run'],
+  #}->
+  #exec { 'ensure_rvmdir_correct_ownership03':
+  #  command => '/bin/chown -R vagrant:rvm /home/vagrant/.rvm',
+  #}-> 
+  #file { '/home/vagrant/.bash_profile':
+  #  ensure => 'present',
+  #}->
+  #file_line { 'write_rvm_default_homebashrc':
+  #  path  => '/home/vagrant/.bash_profile',
+  #  line  => 'rvm use 2.2.3 --default',
+  #  match => '^rvm\ use\ 2\.2\.3*',
+  #}
 
   file { '/home/vagrant/.gemrc':
     ensure => 'present',
@@ -103,12 +117,11 @@ class ruby {
     match => '^gem:\ --no-ri\ --no-rdoc$',
   }->
   exec { 'change_owner_homegemrc':
-    command => '/bin/chown -R vagrant:rvm /home/vagrant/.gemrc',
-    require => Group['rvm'],
+    command => '/bin/chown -R vagrant:vagrant /home/vagrant/.gemrc',
   }->
   exec { 'install_bundler':
     command => 'gem install bundler',
-    path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin', '/home/vagrant/.rvm/bin', '/home/vagrant/.rvm/bin'],
+  #  path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin', '/home/vagrant/.rvm/bin', '/home/vagrant/.rvm/bin'],
   }
 
   ## run rvm-installer
