4512614ae90a5ed4073b801f6c6b7391a3146801
Ismael Angelo A. Casimpan Jr <ismael.angelo@casimpan.com>
2015-10-18 18:25:28 -0700

    removed commented-out sections in discource module + removed unused workaround in ruby

--- puppet/modules/discourse/manifests/init.pp
+++ puppet/modules/discourse/manifests/init.pp
@@ -1,5 +1,4 @@
 class discourse {
-  
   ## git is only needed in getting the discourse source code from github
   package { 'git':
     ensure => 'present',
@@ -29,24 +28,4 @@ class discourse {
     tries   => '5',
     timeout => '0',
   }
-
-  ## gem install bundle --install-dir /home/vagrant/.rvm/gems/ruby-2.2.3
-  #exec { 'install_gem_bundle':
-  #  command  => '/usr/bin/sudo /usr/bin/puppet apply /vagrant/puppet/modules/discourse/_workaround/final_steps.pp',
-  #  creates  => '/home/vagrant/.rvm/gems/ruby-2.2.3/bin/bundle',
-  #  tries    => '5',
-  #}
-  
-  ##
-  ## TODO:
-  ##   1. Make sure the discourse app runs and is accessible from 
-  ##      eth1 ip @port 80
-  ##
-  ##    * cd /opt/discourse/app
-  ##    * bundle install
-  ##    * bundle exec rake db:create db:migrate db:test:prepare
-  ##
-  ##   2. Once above is running, deamonize
-  ##
-
 }
--- puppet/modules/ruby/_workaround/rvm_install.pp
+++
@@ -1,7 +0,0 @@
-## run rvm-installer
-exec { 'rvm_installer_actual':
-   environment => ["HOME=/home/vagrant"],
-   command => '/usr/bin/curl -s https://raw.githubusercontent.com/wayneeseguin/rvm/master/binscripts/rvm-installer|/bin/bash',
-   creates => "/home/vagrant/.rvm/scripts/rvm",
-   user => 'vagrant',
-}
