04af51e6f25d68fb44c01c0427128f262fed1c18
Ismael Casimpan <ismael.angelo@casimpan.com>
2015-10-19 13:28:49 +0800

    added discourse db seeding + made the upstart config working

--- puppet/modules/discourse/manifests/init.pp
+++ puppet/modules/discourse/manifests/init.pp
@@ -27,5 +27,18 @@ class discourse {
     cwd     => "/opt/discourse",
     tries   => '5',
     timeout => '0',
+  }->
+  ## db seeding
+  exec { 'temp_tweak_permission_for_discourseLog':
+    command => '/usr/bin/sudo /bin/chown vagrant:vagrant -R /opt/discourse',
+  }->
+  exec { 'discourse_dbseeding':
+    command => "/usr/bin/sudo /bin/su vagrant -c '/usr/local/bin/bundle exec rake db:create db:migrate db:test:prepare'",
+    cwd     => "/opt/discourse/app",
+    tries   => '5',
+    timeout => '0',
+  }->
+  exec {'set_basic_upstart_config':
+    command => "/usr/bin/sudo /bin/cp /vagrant/puppet/modules/discourse/files/discourse.conf /etc/init/",
   }
 }
